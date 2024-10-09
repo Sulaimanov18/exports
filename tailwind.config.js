d6906e1ae030192bfb2af2bbb64ec02b9d28c116
Sulaimanov18 <sulaimanov1718@gmail.com>
2024-10-09 13:52:15 -0500

    new changess

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,13 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter', 'sans-serif'], // Adding Inter as the default sans-serif font
+        custom: ['SuisseIntl', 'Helvetica Neue', '-apple-system', 'system-ui', 'Segoe UI', 'Roboto', 'Arial', 'sans-serif'],
       },
       colors: {
       'custom-blue': '#477be4',
+      'green' : '#59C65B',
+      'blaa' : '#2B2B2B',
+
     }
     },
   },
